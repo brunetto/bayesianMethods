8438e3b82c149f5396eac1039feac99477c843ae
brunetto <brunetto.ziosi@gmail.com>
2015-03-25 00:17:40 +0100

    working

--- neutrino/bplot2.py
+++ neutrino/bplot2.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*- 
 
-import sys
+import sys, itertools
 import numpy as np
 import veusz.embed as ve
 import matplotlib.pyplot as plt
 
-def sm_hist(data, delta=5, n_bin=None, range_=None):
+def sm_hist(data, delta=5, n_bin=None, range_=None, density=False):
 	dataMin = np.floor(data.min())
 	dataMax = np.ceil(data.max())
 	n_bin = np.ceil(1.*(dataMax-dataMin) / delta)
 	range_ = (dataMin, dataMin + n_bin * delta)
-	counts, bin_edges = np.histogram(data, n_bin, range_, density = False)
+	counts, bin_edges = np.histogram(data, n_bin, range_, density = density)
 	counts = np.hstack((np.array([0]), counts, np.array([0])))
 	bin_edges = np.hstack((bin_edges[0], bin_edges))
 	return counts, bin_edges
@@ -104,20 +104,16 @@ class dataO(object):
 		if self.delta == 0:
 			print "Zero delta, exit"
 			sys.exit(1)
-		self.counts, self.bins = sm_hist(self.data, delta=self.delta)
+		self.counts, self.bins = sm_hist(self.data, delta=self.delta, density=False)
 		self.counts = self.counts / (1.*self.counts.sum()*self.delta)
 	def percentile(self, which):
 		return np.percentile(self.data, which)
 	def myPercentile(self, which):
 		# Wikipedia definition
 		idx = int(np.ceil((which / 100.) * self.data.size))
-		return np.sort(self.data)[idx]
+		return np.sort(self.data)[idx] # non dovrei sortare al contrario mettendo i più alti prima??
 
 infile = sys.argv[1]
-
-document = ve.Embedded("doc_1")
-histPage = document.Root.Add('page')
-contourPage = document.Root.Add('page')
 
 # Importare nell’ambiente grafico files i files di output di JAGS.
 idxType = [("var", "|S30"), ("start","<i8"), ("stop", "<i8")]
@@ -125,18 +121,29 @@ dataType = [("idx", "<i8"), ("data", "<f8")]
 
 idxs = np.genfromtxt("CODAindex.txt", dtype=idxType)
 
+if idxs.size == 1:
+	idxs.shape = (1,1)
+	idxs = idxs[0]
+
 dataAll = np.genfromtxt(infile, dtype=dataType)
 
 data = {}
 
-#for var in [idxs]:
-	#if data.has_key(var[0]):
-		#print var[0], " duplicated! Exit!"
-		#sys.exit(1)
-var="m"
-data[var] = dataO(var, dataAll)
+for var in idxs:
+	if data.has_key(var[0]):
+		print var[0], " duplicated! Exit!"
+		sys.exit(1)
+	data[var[0]] = dataO(var[0], dataAll[var["start"]-1:var["stop"]])
 
-histGridRows = 1#len(idxs)
+document = ve.Embedded("doc_1")
+
+histPageWidth = "20cm"
+histPageHeight = str(10*len(idxs))+"cm"
+
+histPage = document.Root.Add('page', width=histPageWidth, height=histPageHeight)
+contourPage = document.Root.Add('page', width="20cm", height="10cm")
+
+histGridRows = len(idxs)
 histGridColumns = 2
 
 histGrid = histPage.Add('grid', autoadd=False, rows = histGridRows, columns = histGridColumns,
@@ -144,13 +151,13 @@ histGrid = histPage.Add('grid', autoadd=False, rows = histGridRows, columns = hi
 							leftMargin='0cm'
 							)
 
-#contourGridRows = 1
-#contourGridColumns = 1
+contourGridRows = 1
+contourGridColumns = 2
 
-#contourGrid = contourPage.Add('grid', autoadd=False, rows = gridRows, columns = gridColumns,
-							#bottomMargin='2cm',
-							#leftMargin='2.5cm'
-							#)
+contourGrid = contourPage.Add('grid', autoadd=False, rows = contourGridRows, columns = contourGridColumns,
+							bottomMargin='0cm',
+							leftMargin='0cm'
+							)
 
 dataOut = {}
 
@@ -159,7 +166,7 @@ percentile = 95
 for key in data.keys():
 	# Calcolare la media e deviazione standard di una sequenza di valori (p.e. s del file allegato
 	# ha media 28.55 e standard deviation 16.5)
-	print key, data[key].stats()
+	print "Variable ", key, " mean and std ", data[key].stats()
 	
 	# Calcolare l’intervallo pi ́u corto che racchiude l’x % dei valori (p.e. s del file allegato ha
 	# un intervallo al 95 % che va da 0 a 56)
@@ -173,7 +180,7 @@ for key in data.keys():
 	dataOut[data[key].name + ":" + "mean"] = [data[key].mean]
 	dataOut[data[key].name + ":" + "std"] = [data[key].std]
 	dataOut[data[key].name + ":" + "percentile:" + str(percentile)] = [data[key].percentile(percentile)]
-	print data[key].percentile(percentile), data[key].myPercentile(percentile)
+	print "95 percentile", data[key].percentile(percentile), data[key].myPercentile(percentile)
 	dataOut[data[key].name + ":" + "bins"] = data[key].bins
 	dataOut[data[key].name + ":" + "counts"] = data[key].counts
 	
@@ -206,49 +213,51 @@ for key in data.keys():
 	xy.xData.val = dataNameX
 	xy.yData.val = dataNameY
 
-### http://python4mpia.github.io/intro/quick-tour.html
-##hist2D, xedges, yedges = np.histogram2d(data["s"].data, data["bkg"].data, bins=50)
-##hist2D = np.transpose(hist2D)
+if len(idxs) > 1:
+	for combination in itertools.combinations(data.keys(), 2):
+		### http://python4mpia.github.io/intro/quick-tour.html
+		hist2D, xedges, yedges = np.histogram2d(data["s"].data, data["bkg"].data, bins=50)
+		hist2D = np.transpose(hist2D)
 
-### Colormap
-##graph = contourGrid.grid.Add('graph', autoadd=False)
-##xAxis = graph.Add('axis', name='x', label = "")
-##yAxis = graph.Add('axis', name='y', label = "")
-##xy = graph.Add('xy')
+		### Colormap
+		graph = contourGrid.Add('graph', autoadd=False)
+		xAxis = graph.Add('axis', name='x', label = "")
+		yAxis = graph.Add('axis', name='y', label = "")
+		xy = graph.Add('image')
+					
+		dataName = "hist2D"      
+		document.SetData2D(dataName, hist2D)
+		xy.data.val = dataName
+
+
+		np.savetxt("joint.csv", hist2D, delimiter=",")
+
+		###plt.pcolormesh(xedges, yedges, hist2D, cmap=plt.cm.gray)
+
+		graph = contourGrid.Add('graph', autoadd=False)
+		xAxis = graph.Add('axis', name='x', label = "")
+		yAxis = graph.Add('axis', name='y', label = "")
+
+		### Find levels
+		for level in [0.68, 0.95]:
+			threshold = findLevel(hist2D, level)
+			tmp = plt.contour(hist2D, extent=[xedges.min(),xedges.max(),yedges.min(),yedges.max()], levels=[threshold]).collections[0].get_paths()
+			dataOut["level_"+str(level)] = [int(threshold)]
 			
-##dataNameX = data[key].name + ":" + "idxs"      
-##dataNameY = data[key].name + ":" + "data"
-##document.SetData(dataNameX, data[key].idxs)
-##document.SetData(dataNameY, data[key].data)
-##xy.xData.val = dataNameX
-##xy.yData.val = dataNameY
-
-##np.savetxt("joint.csv", hist2D, delimiter=",")
-
-###plt.pcolormesh(xedges, yedges, hist2D, cmap=plt.cm.gray)
-
-### Find levels
-##for level in [0.68, 0.95]:
-	##threshold = findLevel(hist2D, level)
-	##tmp = plt.contour(hist2D, extent=[xedges.min(),xedges.max(),yedges.min(),yedges.max()], levels=[threshold]).collections[0].get_paths()
-	##dataOut["level_"+str(level)] = [int(threshold)]
-	
-	##graph = contourGrid.grid.Add('graph', autoadd=False)
-	##xAxis = graph.Add('axis', name='x', label = "")
-	##yAxis = graph.Add('axis', name='y', label = "")
-	
-	##for idx in range(len(tmp)):
-		##dataOut["level_"+str(level)+"_contour_x_"+str(idx)] = tmp[idx].vertices[:,0]
-		##dataOut["level_"+str(level)+"_contour_y_"+str(idx)] = tmp[idx].vertices[:,1]
-		##xy = graph.Add('xy')		
-		
-		##dataNameX = "level_"+str(level)+"_contour_x_"+str(idx)
-		##dataNameY = "level_"+str(level)+"_contour_y_"+str(idx)
-		##document.SetData(dataNameX, tmp[idx].vertices[:,0])
-		##document.SetData(dataNameY, tmp[idx].vertices[:,1])
-		##xy.xData.val = dataNameX
-		##xy.yData.val = dataNameY
-		
+			
+			
+			for idx in range(len(tmp)):
+				dataOut["level_"+str(level)+"_contour_x_"+str(idx)] = tmp[idx].vertices[:,0]
+				dataOut["level_"+str(level)+"_contour_y_"+str(idx)] = tmp[idx].vertices[:,1]
+				xy = graph.Add('xy', marker = 'none')		
+				
+				dataNameX = "level_"+str(level)+"_contour_x_"+str(idx)
+				dataNameY = "level_"+str(level)+"_contour_y_"+str(idx)
+				document.SetData(dataNameX, tmp[idx].vertices[:,0])
+				document.SetData(dataNameY, tmp[idx].vertices[:,1])
+				xy.xData.val = dataNameX
+				xy.yData.val = dataNameY
+				
 
 ###plt.show()
 
